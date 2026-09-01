15b8e3cfe0c454075d9fec22b708facdbb74a3cd
Alvaro <alvamarblan@gmail.com>
2026-09-01 13:09:13 +0000

    Mapa: usar tiles OSM sin API key con filtro oscuro de marca

--- components/MapClient.tsx
+++ components/MapClient.tsx
@@ -67,8 +67,9 @@ export default function MapClient() {
         className="w-full h-full bg-background"
       >
         <TileLayer
-          attribution='&copy; <a href="https://carto.com/attributions">CARTO</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          className="map-tiles-dark"
         />
         <FlyToCity target={selected} />
         <ZoomWatcher onZoom={setZoom} />
